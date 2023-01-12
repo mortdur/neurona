4dda24ebf660bd34aa257f0d0124c4d59c2f5a79
mortdur <121159231+mortdur@users.noreply.github.com>
2023-01-12 17:40:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,52 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import joblib
-st.image("neurona.jpg", width=450)
-st.title("¡Hola neurona!")
-tab1, tab2, tab3 = st.tabs(["Una entrada", "Dos entradas", "Tres entrada y sesgo"])
 
-with tab1:
-  st.title("Una neurona con una entrada y un peso")
-  x = st.slider("Peso:",0.0, 5.0)
-  w = st.number_input("Valor de entrada")
+def relu(x):
+  return np.maximum(0, x)
+
+def sig(x):
+ return 1/(1 + np.exp(-x))
+
+def hip(x):
+  return np.tanh(x)
+
+class Neuron():
+  def __init__(self,weights, bias, func):
+    self.weights = weights
+    self.bias = bias
+    self.func = func
   
-  if st.button("Calcular la salida",key="boton1"):
-    y = x * w
-    st.text(f"Resultado {y}")
+  def run(self, input_data):
+    x = np.array(input_data)
+    calc = np.dot(self.weights, x) + self.bias
+    if self.func == "ReLu":
+      self.func = relu
+    elif self.func == "Sigmoide":
+      self.func = sig
+    elif self.func == "Tangente Hipervolica":
+      self.func = hip
+    return self.func(calc)
+st.image("neurona.jpg", width=450)
+st.title("Simulador de neurona")
 
-with tab2:
-  col1, col2 = st.columns(2)
-  with col1:
-    x0 = st.slider("Peso w0:",0.0, 5.0,key="x0")
-    w0 = st.number_input("Entrada x0",key="w0")
-  with col2:
-    x1 = st.slider("Peso w0:",0.0, 5.0,key="x1")
-    w1 = st.number_input("Entrada x1",key="w1")
+c = st.slider("Indica el numero de entradas y pesos:",0.0, 5.0)
 
-  if st.button("Calcular la salida",key="salida2"):
-    y = x0 * w0 + x1 * w1
-    st.text(f"Resultado {y}")
+st.title("¡Entradas!")
+x = st.number_input("Entrada x")
 
-with tab3:
-  col1, col2, col3 = st.columns(3)
-  with col1:
-    x0 = st.slider("Peso w0:",0.0, 5.0,key="x00")
-    w0 = st.number_input("Entrada x0",key="w00")
-  with col2:
-    x1 = st.slider("Peso w1:",0.0, 5.0,key="x01")
-    w1 = st.number_input("Entrada x1",key="w01")
-  with col3:
-    x2 = st.slider("Peso w2:",0.0, 5.0,key="x02")
-    w2 = st.number_input("Entrada x2",key="w02")
-  b = st.number_input("Introduzca el valor del sesgo")
-    
-  if st.button("Calcular la salida",key="salida3"):
-    y = x0 * w0 + x1 * w1 + x2 * w2 + b
-    st.text(f"Resultado {y}")
+st.title("¡Pesos!")
+weights = st.number_input("Entrada w")
+
+col1, col2 = st.columns(2)
+with col1:
+  bias = st.number_input("Introduzca el valor del sesgo")
+with col2:
+  func = st.selectbox('Selecciona funcion de activacion',("Sigmoide", "ReLu", "Tangente Hipervolica" ))
+      
+if st.button("Calcular la salida"):
+  miNeurona = Neuron(weights, bias, func)
+  output = miNeurona.run(input_data = x)
+  print(output)
+  
